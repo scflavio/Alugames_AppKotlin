0a73bb39042b13d560edd791bda0bfe43371b54c
Flavio Santos Costa <c093315@corp.caixa.gov.br>
2024-02-23 13:06:32 -0300

    versão inicial

---
+++ app/src/main/java/br/com/fsc/orgs/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.com.fsc.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity: Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
